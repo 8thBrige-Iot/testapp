468e596973380a9be205f52011eba53ca8708fce
Philip Silva <psilva.de@gmail.com>
2014-10-25 21:38:29 +0200

    Some debug information

--- public/js/main.js
+++ public/js/main.js
@@ -21,9 +21,6 @@ var transform = function(lon, lat) {
   var r = (p0.lon - p1.lon) / (p0.x - p1.x);
   var s = (p0.lat - p1.lat) / (p0.y - p1.y);
 
-  console.log('r = ', r);
-  console.log('s = ', s);
-
   return { // -(10, 10) because of circle radius = 10
     x: (lon - p0.lon) / r + p0.x - 10,
     y: (lat - p0.lat) / s + p0.y - 10
@@ -37,16 +34,13 @@ $.getJSON('/api/districts', function(ds) {
     $districts.append('<p>' + d.name + '</p>');
   });
 
-  var p0t = transform(p0.lon, p0.lat);
-  console.log('p0t:');
-  console.log(p0t);
-  var p1t = transform(p1.lon, p1.lat);
-  console.log('p1t:');
-  console.log(p1t);
-
   var $map = $('#map');
   _.each(ds, function(d) {
-    var p = transform(d.lon, d.lat);
-    $map.append('<div class="marker" title="' + d.name + '" style="position: absolute; margin-top: ' + p.y + 'px; margin-left: ' + p.x + 'px"></div>');
+    if (d.lon && d.lat) {
+      var p = transform(d.lon, d.lat);
+      $map.append('<div class="marker" title="' + d.name + '" style="position: absolute; margin-top: ' + p.y + 'px; margin-left: ' + p.x + 'px"></div>');
+    } else {
+      console.log('no coords for ', d.name);
+    }
   });
 });
