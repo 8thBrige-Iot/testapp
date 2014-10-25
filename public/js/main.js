6c45efaf2737cb980b9775d58d2ab54dbb20466b
Philip Silva <psilva.de@gmail.com>
2014-10-25 22:57:47 +0200

    large dots for whole districts

--- public/js/main.js
+++ public/js/main.js
@@ -17,13 +17,15 @@ var p0 = fixPoints[0];
 var p1 = fixPoints[1];
 
 //                       "x", "y"
-var transform = function(lon, lat) {
+var transform = function(lon, lat, isLarge) {
   var r = (p0.lon - p1.lon) / (p0.x - p1.x);
   var s = (p0.lat - p1.lat) / (p0.y - p1.y);
 
+  var radius = isLarge ? 20 : 10;
+
   return { // -(10, 10) because of circle radius = 10
-    x: (lon - p0.lon) / r + p0.x - 10,
-    y: (lat - p0.lat) / s + p0.y - 10
+    x: (lon - p0.lon) / r + p0.x - radius,
+    y: (lat - p0.lat) / s + p0.y - radius
   }
 };
 
@@ -37,8 +39,14 @@ $.getJSON('/api/districts', function(ds) {
   var $map = $('#map');
   _.each(ds, function(d) {
     if (d.lon && d.lat) {
-      var p = transform(d.lon, d.lat);
-      $map.append('<div class="marker" title="' + d.name + '" style="position: absolute; margin-top: ' + p.y + 'px; margin-left: ' + p.x + 'px"></div>');
+      var isLarge = d.name[0] == '1';
+      var p = transform(d.lon, d.lat, isLarge);
+      var className = 'marker ';
+      if (isLarge) {
+        className += 'large';
+
+      }
+      $map.append('<div class="' + className + '" title="' + d.name.slice(1) + '" style="position: absolute; margin-top: ' + p.y + 'px; margin-left: ' + p.x + 'px"></div>');
     } else {
       console.log('no coords for ', d.name);
     }
